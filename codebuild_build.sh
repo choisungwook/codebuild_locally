1e4602e0726f854cd82353d701e63144d0d7f27d
choisungwook <kgg1959@naver.com>
2023-11-04 22:35:14 +0900

    Add: docker container run option

--- codebuild_build.sh
+++ codebuild_build.sh
@@ -96,7 +96,7 @@ then
     exit 1
 fi
 
-docker_command="docker run -it "
+docker_command="docker run --rm -it "
 if isOSWindows
 then
     docker_command+="-v //var/run/docker.sock:/var/run/docker.sock -e "
